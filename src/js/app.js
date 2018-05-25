b8ae4dd78e8cab38ea7d07b086501ff70ded7a1c
viktormovchaniuk <viktormovchaniuk@gmail.com>
2018-05-25 18:28:29 +0300

    day3

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,30 @@ jQueryBridget( 'isotope', Isotope, $ );
     });
   });
 
+  $(window).resize(function() {
+    $(window).width() > 900 && $('.main-nav').removeAttr('style') && menuBtn.removeClass('is-active');
+  });
+
+
+
+  
+  // var modal = $('.modal');
+  
+
+  // $('#modal-btn').on('click', function() {
+  //   modal.fadeIn();
+  //   $('body').addClass('modal-open').prepend('<div class="modal-overlay"></div>');
+  // });
+
+  // $('#btn-close').on('click', function() {
+  //   console.log(this);
+  //   modal.fadeOut();
+  //   $('body').removeClass('modal-open');
+  //   $('.modal-overlay').fadeOut( function() {
+  //     $(this).remove();
+  //   });
+  // });
+
 }());
 
 var selectedCategory;
@@ -33,41 +57,50 @@ var $grid = $('.grid').isotope({
   },
   getSortData: {
     selectedCategory: function( itemElem ) {
-      return $( itemElem ).hasClass( selectedCategory ) ? 0 : 1;
+      return $( itemElem ).hasClass( 'new' ) ? 0 : 1;
     }
   }
 });
 
-var $items = $('.grid').find('.grid-item');
+// var $items = $('.grid').find('.grid-item');
 
 $('#filters').on( 'click', 'button', function() {
-
-  selectedCategory = $( this ).attr('data-sort-by');
-  if ( selectedCategory == 'all' ) {
-    $grid.isotope({
-      sortBy: 'original-order'
-    });
-    // restore all items to full opacity
-    $items.css({
-      opacity: 1
-    });
-    return;
-  }
-  // change opacity for selected/unselected items
-  var selectedClass = '.' + selectedCategory;
-  $items.filter( selectedClass ).find('.card__header-text').css({
-    backgroundColor: '#faac03'
-  });
-  $items.not( selectedClass ).find('.card__header-text').css({
-    backgroundColor: '#aaaaaa'
-  });
-
-  // update sort data now that selectedCategory has changed
+  var filterValue = $( this ).attr('data-filter');
+  console.log(filterValue);
+  // use filterFn if matches value
+  // filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
   $grid.isotope('updateSortData');
   $grid.isotope({ sortBy: 'selectedCategory' });
 });
 
-// change is-checked class on buttons
+// $('#filters').on( 'click', 'button', function() {
+
+//   selectedCategory = $( this ).attr('data-sort-by');
+//   if ( selectedCategory === 'all' ) {
+//     $grid.isotope({
+//       sortBy: 'original-order'
+//     });
+//     // restore all items to full opacity
+//     $items.css({
+//       opacity: 1
+//     });
+//     return;
+//   }
+//   // change opacity for selected/unselected items
+//   var selectedClass = '.' + selectedCategory;
+//   $items.filter( selectedClass ).find('.card__header-text').css({
+//     backgroundColor: '#faac03'
+//   });
+//   $items.not( selectedClass ).find('.card__header-text').css({
+//     backgroundColor: '#aaaaaa'
+//   });
+
+//   // update sort data now that selectedCategory has changed
+//   
+// });
+
+// // change is-checked class on buttons
 $('#filters').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
   $buttonGroup.on( 'click', 'button', function() {
@@ -76,32 +109,31 @@ $('#filters').each( function( i, buttonGroup ) {
   });
 });
 
-var $newItems = $(`
-<div class="grid-item access">
-  <div class="card">
-      <div class="card__header">
-        <span class="card__header-text">access111</span>
-      </div>
-      <div class="card__body">
-        <h4 class="card__body-title">Improve the logic of collecting resources with an inventory tool available</h4>
-        <p class="card__body-text"> </p>
-      </div>
-  </div>
-</div>`);
-setTimeout(function() {
-  $('.grid').append( $newItems).isotope( 'reloadItems' ).isotope({ sortBy: 'selectedCategory' });
-  $items = $('.grid').find('.grid-item');
+// var $newItems = $(`
+// <div class="grid-item access">
+//   <div class="card">
+//       <div class="card__header">
+//         <span class="card__header-text">access111</span>
+//       </div>
+//       <div class="card__body">
+//         <h4 class="card__body-title">Improve the logic of collecting resources with an inventory tool available</h4>
+//         <p class="card__body-text"> </p>
+//       </div>
+//   </div>
+// </div>`);
+// setTimeout(function() {
+//   $('.grid').append( $newItems).isotope( 'reloadItems' ).isotope({ sortBy: 'selectedCategory' });
+//   $items = $('.grid').find('.grid-item');
 
-  var selectedClass = '.' + selectedCategory;
-  $items.filter( selectedClass ).find('.card__header-text').css({
-    backgroundColor: '#faac03'
-  });
-  $items.not( selectedClass ).find('.card__header-text').css({
-    backgroundColor: '#aaaaaa'
-  });
-}, 5000);
+//   var selectedClass = '.' + selectedCategory;
+//   $items.filter( selectedClass ).find('.card__header-text').css({
+//     backgroundColor: '#faac03'
+//   });
+//   $items.not( selectedClass ).find('.card__header-text').css({
+//     backgroundColor: '#aaaaaa'
+//   });
+// }, 5000);
 
-console.log(Isotope);
 
 
 
--- src/js/lib/dropdown.js
+++
@@ -1,3 +0,0 @@
-import Popper from 'popper.js';
-import Util from './util';
-import Dropdown from './dropdown';
